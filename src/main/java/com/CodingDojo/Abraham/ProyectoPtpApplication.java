73215d2213720b9996a09948404f271da0a4db6a
Abraham Fortunatte <a.fortunatte@gmail.com>
2023-10-02 20:31:17 -0300

    creacion del proyecto

---
+++ src/main/java/com/CodingDojo/Abraham/ProyectoPtpApplication.java
@@ -0,0 +1,13 @@
+package com.CodingDojo.Abraham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodingDojo/Abraham/ProyectoPtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodingDojo.Abraham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
